5ec849d78f3d696443e3c4b1d2897fb8d12a829f
manuelbarzi <manuelbarzi@gmail.com>
2025-10-02 16:53:26 +0200

    mechanize login form; add navigation from login to register, and from register to login

--- staff/manuel-barzi/product/app/main.js
+++ staff/manuel-barzi/product/app/main.js
@@ -1,4 +1,9 @@
-var registerForm = document.getElementById('register-form')
+// register
+
+var registerView = document.querySelector('#registerView')
+registerView.style.display = 'none'
+
+var registerForm = registerView.querySelector('#registerForm')
 
 registerForm.addEventListener('submit', function (event) {
     event.preventDefault()
@@ -10,3 +15,41 @@ registerForm.addEventListener('submit', function (event) {
 
     logic.registerUser(fullName, dateOfBirth, email, password)
 })
+
+var loginLink = registerView.querySelector('#loginLink')
+
+loginLink.addEventListener('click', function (event) {
+    event.preventDefault()
+
+    registerView.style.display = 'none'
+    loginView.style.display = 'block'
+})
+
+// login
+
+var loginView = document.querySelector('#loginView')
+
+var loginForm = loginView.querySelector('#loginForm')
+
+loginForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var email = loginForm.email.value
+    var password = loginForm.password.value
+
+    logic.loginUser(email, password)
+})
+
+var registerLink = loginView.querySelector('#registerLink')
+
+registerLink.addEventListener('click', function (event) {
+    event.preventDefault()
+
+    loginView.style.display = 'none'
+    registerView.style.display = 'block'
+})
+
+// home
+
+var homeView = document.querySelector('#homeView')
+homeView.style.display = 'none'
